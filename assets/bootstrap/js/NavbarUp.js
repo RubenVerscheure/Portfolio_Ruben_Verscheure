754874371d957f1daa74c6144ff2eea959794258
ruversc <ruben.verscheure@ugent.be>
2023-05-30 19:27:45 +0200

    Annotate code

--- assets/bootstrap/js/NavbarUp.js
+++ assets/bootstrap/js/NavbarUp.js
@@ -1,12 +1,12 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navcol-1')
-const bsCollapse = new bootstrap.Collapse(menuToggle, {
+const navLinks = document.querySelectorAll('.nav-item') //select all items from class and store
+const menuToggle = document.getElementById('navcol-1') //select element
+const bsCollapse = new bootstrap.Collapse(menuToggle, { //make sure menu starts collapsed when new constant is added
   toggle: false
 })
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => {
-        if(bsCollapse._isShown()){
-            bsCollapse.hide()
+navLinks.forEach((l) => { //iterate for every link in the navbar
+    l.addEventListener('click', () => { //checks if link is clicked
+        if(bsCollapse._isShown()){ //given that navbar is shown
+            bsCollapse.hide() // close on click
         }
     })
 })
